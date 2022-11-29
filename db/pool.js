b8240eae4763a3ed22ed7d24194920122c406786
akashpunagin <akash.punagin@gmail.com>
2022-11-29 18:49:27 +0530

    edit pool.js

--- db/pool.js
+++ db/pool.js
@@ -14,8 +14,11 @@ const isProdMode = () => {
 
 const pool = isProdMode()
   ? new Pool({
-      connectionString: process.env.DATABASE_URL,
-      ssl: { rejectUnauthorized: false },
+      host: process.env.DB_HOST,
+      port: process.env.DB_PORT,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
     })
   : new Pool({
       user: "me",
