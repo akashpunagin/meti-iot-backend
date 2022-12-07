dd78c9e0c683a04c2bf214423023d3b4b843567f
akashpunagin <akash.punagin@gmail.com>
2022-12-07 11:01:29 +0530

    get data add order by timestampz

--- routes/sensorValue/sensorValueRoutes/getData.js
+++ routes/sensorValue/sensorValueRoutes/getData.js
@@ -18,6 +18,7 @@ module.exports = (router) => {
         v.device_id = m.device_id AND
         v.sensor_idx = m.sensor_idx AND
         v.device_id = $1
+      ORDER BY v.reading_time
       `,
         [deviceId]
       );
