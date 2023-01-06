f5df160b3b2e1c5df665bb6a1e69760bc919cf67
akashpunagin <akash.punagin@gmail.com>
2023-01-06 12:20:07 +0530

    tenent: get assigned devices

---
+++ routes/tenant/tenantRoutes/get-assigned-devices.js
@@ -0,0 +1,28 @@
+const pool = require("../../../../../db/pool");
+const {
+  authorization,
+} = require("../../../../../middleware/exportMiddlewares");
+const appConstants = require("../../../../../constants/appConstants");
+
+module.exports = (router) => {
+  router.post("/get-assigned-devices", [authorization], async (req, res) => {
+    console.log("Route:", req.originalUrl);
+
+    const { deviceTenant } = appConstants.SQL_TABLE;
+    const tenet = req.user;
+
+    try {
+      const addRes = await pool.query(
+        `SELECT FROM ${deviceTenant}
+        WHERE tenet_id = $1`,
+        [tenet.userId]
+      );
+
+      const data = addRes.rows;
+      return res.status(200).json(data);
+    } catch (error) {
+      console.log("GET DEVICE TenantS ERROR", error);
+      return res.status(500).json("Server error");
+    }
+  });
+};
