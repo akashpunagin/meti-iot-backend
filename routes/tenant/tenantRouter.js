0f44d6a93d37c1e31b40863a6841e6604d2aca14
akashpunagin <akash.punagin@gmail.com>
2023-01-06 12:23:29 +0530

    tenent: router: add get assigned device route

--- routes/tenant/tenantRouter.js
+++ routes/tenant/tenantRouter.js
@@ -2,5 +2,6 @@ const router = require("express").Router();
 
 require("./tenantRoutes/get")(router);
 require("./tenantRoutes/get-all")(router);
+require("./tenantRoutes/get-assigned-devices")(router);
 
 module.exports = router;
