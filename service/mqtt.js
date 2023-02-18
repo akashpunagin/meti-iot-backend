1a30f197a9945d0261a57c4b8d87631664436543
akashpunagin <akash.punagin@gmail.com>
2023-02-18 10:27:24 +0530

    change subscribe code 5

--- service/mqtt.js
+++ service/mqtt.js
@@ -94,7 +94,8 @@ async function saveDataFromTopics(deviceIdtopicsObjs) {
     //   }`
     // );
 
-    console.log("ON MESSAGE: PAYLOAD:", payload);
+    console.log({ topic, packet });
+    console.log("ON MESSAGE: PAYLOAD:", JSON.parse(payload.toString()));
     // saveDataInDatabase(deviceIdtopicsObj, JSON.parse(payload.toString()));
     // client.end();
   });
